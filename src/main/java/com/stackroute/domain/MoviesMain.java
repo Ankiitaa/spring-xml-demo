ee6752d62025be7bc8aa835a92fc9e7e1aebff1e
Ankiitaa <ankitahs08@gmail.com>
2019-03-04 09:46:25 +0530

    autowire

--- src/main/java/com/stackroute/domain/MoviesMain.java
+++ src/main/java/com/stackroute/domain/MoviesMain.java
@@ -8,8 +8,8 @@ import org.springframework.core.io.ClassPathResource;
 public class MoviesMain {
     public static void main(String[] args) {
         ApplicationContext bean=new ClassPathXmlApplicationContext("bean.xml");
-        XmlBeanFactory factory=new XmlBeanFactory(new ClassPathResource("bean.xml"));
-        Movie movie1=(Movie)factory.getBean("movie1");
+        ApplicationContext bean1=new ClassPathXmlApplicationContext("bean.xml");
+        Movie movie1=(Movie)bean1.getBean("movie1");
         Movie movie =(Movie)bean.getBean("movie");
         System.out.println(movie.getActor());
         System.out.println(movie1.getActor());
