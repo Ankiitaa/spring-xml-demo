740caec8acb5471655650d6308827d83ce42299c
Ankiitaa <ankitahs08@gmail.com>
2019-03-05 09:38:39 +0530

    lifecycle

--- src/main/java/com/stackroute/domain/MoviesMain.java
+++ src/main/java/com/stackroute/domain/MoviesMain.java
@@ -1,5 +1,6 @@
 package com.stackroute.domain;
 
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,12 +8,12 @@ import org.springframework.core.io.ClassPathResource;
 
 public class MoviesMain {
     public static void main(String[] args) {
-        ApplicationContext bean=new ClassPathXmlApplicationContext("bean.xml");
-        ApplicationContext bean1=new ClassPathXmlApplicationContext("bean.xml");
-        Movie movie1=(Movie)bean1.getBean("movie1");
-        Movie movie =(Movie)bean.getBean("movie");
-        System.out.println(movie.getActor());
-        System.out.println(movie1.getActor());
 
+        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+        BeanLifecycleDemoBean beanLifecycleDemoBean = (BeanLifecycleDemoBean)context.getBean("BeanLifecycle");
+        beanLifecycleDemoBean.display();
+        beanLifecycleDemoBean.customInit();
+        beanLifecycleDemoBean.customDestroy();
     }
+
 }
